5f2d2bbed159858b15e6bcb93343d9489d947e16
Bryan <bryan.pilco@gmail.com>
2018-10-12 10:22:14 -0500

    Ejercicio 1 de la clase 2

---
+++ Clase2/src/clase2/Ejecutor.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clase2;
+
+/**
+ *
+ * @author Bryan
+ */
+public class Ejecutor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Precio p;
+        p = new Precio();
+        System.out.println(p.euro);
+        
+    }
+    
+}
---
+++ Clase2/src/clase2/Precio.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clase2;
+
+/**
+ *
+ * @author Bryan
+ */
+public class Precio {
+    
+    double euro;
+    
+    public double obtener(){
+        return 0;
+    }
+    
+    public void agregar(double x){
+        
+    }
+    
+}
